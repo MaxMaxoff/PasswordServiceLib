f31eb1c96cbcac60374670228481531c445510e9
Maxim Toropov <maxim.vt@gmail.com>
2019-02-08 20:26:42 +0300

    Добавьте файлы проекта.

---
+++ PasswordServiceLib/PasswordEncoder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordServiceLib
+{
+    public static class PasswordEncoder
+    {
+        public static string Encrypt(string str, int key = 1)
+        {
+            return new string(str.Select(c => (char)(c + key)).ToArray());
+        }
+
+        public static string Decrypt(string str, int key = 1)
+        {
+            return new string(str.Select(c => (char)(c - key)).ToArray());
+        }
+
+    }
+}
---
+++ PasswordServiceLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("PasswordServiceLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("PasswordServiceLib")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM, установите атрибут ComVisible для этого типа в значение true.
+[assembly: ComVisible(false)]
+
+// Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+[assembly: Guid("bbe5bee5-2f4e-4a37-8e3d-2134bb4df57a")]
+
+// Сведения о версии для сборки включают четыре следующих значения:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// Используйте для этого символ "*", следующим образом.
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
